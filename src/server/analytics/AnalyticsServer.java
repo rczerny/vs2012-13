99f947e5cadd96bbb5a995f30520ffe557aa2501
Daniel <Daniel@Daniel-PC>
2012-11-11 19:34:31 +0100

    AnalyticsServer method structure added started to implement filter

--- src/server/analytics/AnalyticsServer.java
+++ src/server/analytics/AnalyticsServer.java
@@ -1,6 +1,8 @@
 package server.analytics;
 
-public class AnalyticsServer {
+import java.rmi.RemoteException;
+
+public class AnalyticsServer implements AnalyticsServerRMI{
 
 	/**
 	 * @param args
@@ -8,6 +10,24 @@ public class AnalyticsServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public String subcribe(String filter) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processEvent(Event e) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unsubcribe(String identifier) throws RemoteException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ src/server/analytics/AnalyticsServerRMI.java
@@ -0,0 +1,14 @@
+package server.analytics;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Interface for ensuring RMI methods important for the analytics server.
+ *
+ */
+public interface AnalyticsServerRMI extends Remote{
+	String subcribe(String filter) throws RemoteException;
+	void processEvent(Event e) throws RemoteException;
+	void unsubcribe(String identifier) throws RemoteException;
+}
